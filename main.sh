677d6cf57d4e77660328da11967b2f460cfbc79c
Swiss professional sh!ta$$ <76264518+kevadesu@users.noreply.github.com>
2023-06-09 21:20:24 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -14,13 +14,32 @@ echo "
  |_____/|_| |_|\___|_|_|\___\___| .__/ \__|_|\___/|_| |_|
  A shell inside a shell         | |                      
                                 |_|"
+
 shcept_main
 }
 
 shcept_main() {
-echo "╭"$PWD
-read -p "╰» " SHCEPT_ANSWER
-$SHCEPT_ANSWER
+echo "┌"$PWD
+echo "├/"$USER
+read -p "└" SHCEPT_ANSWER
+case "$SHCEPT_ANSWER" in
+	zhcpt-e)
+		echo -e "#!/bin/bash\n# This file is important if you want to run multiple commands at once in Shellception.\n# This file gets wiped every time you edit it. To keep the contents of the file,\n# run 'zhcpt-e-k' instead to edit the file while keeping the content aswell. To run the script, type 'zhcpt-r'." > ~/zhcpt.sh
+		nano ~/zhcpt.sh
+		chmod +x ~/zhcpt.sh
+	;;
+	zhcpt-e-k)
+		nano ~/zhcpt.sh
+		chmod +x ~/zhcpt.sh
+	;;
+	zhcpt-r)
+		chmod +x ~/zhcpt.sh
+		bash ~/zhcpt.sh
+	;;
+	*)
+		$SHCEPT_ANSWER || shcept_main
+	;;
+esac
 shcept_main
 }
 
